8a5f07748420d92a14cb0d4767968638f69c3f30
greenart7c3 <greenart7c3@proton.me>
2024-02-12 20:37:00 -0300

    0.0.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.greenart7c3.citrine"
         minSdk = 26
         targetSdk = 34
-        versionCode = 2
-        versionName = "0.0.2"
+        versionCode = 3
+        versionName = "0.0.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
--- app/src/main/java/com/greenart7c3/citrine/EventSubscription.kt
+++ app/src/main/java/com/greenart7c3/citrine/EventSubscription.kt
@@ -7,10 +7,6 @@ import com.greenart7c3.citrine.database.AppDatabase
 import io.ktor.server.websocket.DefaultWebSocketServerSession
 import io.ktor.websocket.send
 import kotlinx.coroutines.DelicateCoroutinesApi
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 import java.util.concurrent.ConcurrentHashMap
 
@@ -67,30 +63,30 @@ object EventSubscription {
             }
             session.send(EOSE(subscriptionId).toJson())
 
-            GlobalScope.launch(Dispatchers.IO) {
-                delay(5000)
-                subscriptions[subscriptionId] = Subscription(
-                    subscriptionId,
-                    session,
-                    filters.map {
-                        val since = it.since?.plus(1) ?: (System.currentTimeMillis() / 1000).toInt()
-                        it.since = since
-                        it
-                    }.toSet()
-                )
-
-                val subscription = subscriptions[subscriptionId]
-                if (subscription != null) {
-                    subscribe(
-                        subscription.id,
-                        subscription.filters,
-                        subscription.session,
-                        appDatabase,
-                        objectMapper,
-                        true
-                    )
-                }
-            }
+//            GlobalScope.launch(Dispatchers.IO) {
+//                delay(5000)
+//                subscriptions[subscriptionId] = Subscription(
+//                    subscriptionId,
+//                    session,
+//                    filters.map {
+//                        val since = it.since?.plus(1) ?: (System.currentTimeMillis() / 1000).toInt()
+//                        it.since = since
+//                        it
+//                    }.toSet()
+//                )
+//
+//                val subscription = subscriptions[subscriptionId]
+//                if (subscription != null) {
+//                    subscribe(
+//                        subscription.id,
+//                        subscription.filters,
+//                        subscription.session,
+//                        appDatabase,
+//                        objectMapper,
+//                        true
+//                    )
+//                }
+//            }
         }
     }
 }
