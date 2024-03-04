7e6f42b16a9e93ecd71338c5ea258a9828919b4d
greenart7c3 <greenart7c3@proton.me>
2024-03-04 12:07:06 -0300

    Move project to version catalogs

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,4 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
-buildscript {
-    extra.apply {
-        set("room_version", "2.6.1")
-        set("ktor_version", "2.3.8")
-        set("compose_ui_version", "1.6.2")
-    }
-}
-
 plugins {
     id("com.android.application") version "8.3.0" apply false
     id("org.jetbrains.kotlin.android") version "1.9.22" apply false
