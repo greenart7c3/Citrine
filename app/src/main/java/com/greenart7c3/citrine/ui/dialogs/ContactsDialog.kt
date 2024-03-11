0b947f07156dcfa3879fdd00477865a016e173a6
greenart7c3 <greenart7c3@proton.me>
2024-03-11 11:05:32 -0300

    add a text when no follow list is found

--- app/src/main/java/com/greenart7c3/citrine/ui/dialogs/ContactsDialog.kt
+++ app/src/main/java/com/greenart7c3/citrine/ui/dialogs/ContactsDialog.kt
@@ -29,9 +29,12 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Dialog
 import androidx.compose.ui.window.DialogProperties
+import com.greenart7c3.citrine.R
 import com.greenart7c3.citrine.database.AppDatabase
 import com.greenart7c3.citrine.database.toEvent
 import com.greenart7c3.citrine.relays.Client
@@ -137,6 +140,19 @@ fun ContactsDialog(pubKey: String, onClose: () -> Unit) {
                         CloseButton(
                             onCancel = onClose
                         )
+                    }
+
+                    if (events.isEmpty()) {
+                        Column(
+                            Modifier.fillMaxSize(),
+                            verticalArrangement = Arrangement.Center,
+                            horizontalAlignment = Alignment.CenterHorizontally
+                        ) {
+                            Text(
+                                stringResource(R.string.no_follow_list_found),
+                                textAlign = TextAlign.Center
+                            )
+                        }
                     }
 
                     LazyColumn(
