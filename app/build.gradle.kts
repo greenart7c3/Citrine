0de9fb87ac7f2655de2e9b2dd4163beceb22403a
greenart7c3 <greenart7c3@proton.me>
2024-04-24 13:53:10 -0300

    update kotlinCompilerExtensionVersion

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,7 +52,7 @@ android {
         buildConfig = true
     }
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.8"
+        kotlinCompilerExtensionVersion = "1.5.12"
     }
     packaging {
         resources {
