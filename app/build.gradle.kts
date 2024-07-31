ddebca7453dbe844a9aa5d12153f0ab10137241f
greenart7c3 <greenart7c3@proton.me>
2024-07-31 18:34:13 -0300

    0.3.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.greenart7c3.citrine"
         minSdk = 26
         targetSdk = 34
-        versionCode = 25
-        versionName = "0.3.1"
+        versionCode = 26
+        versionName = "0.3.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
