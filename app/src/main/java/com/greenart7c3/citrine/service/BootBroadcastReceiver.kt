cf29e4e1006c1a3e908181d13249b05b8bc953a4
greenart7c3 <greenart7c3@proton.me>
2024-10-25 13:02:59 -0300

    Start service after update

--- app/src/main/java/com/greenart7c3/citrine/service/BootBroadcastReceiver.kt
+++ app/src/main/java/com/greenart7c3/citrine/service/BootBroadcastReceiver.kt
@@ -3,10 +3,17 @@ package com.greenart7c3.citrine.service
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.os.Build
 
 class BootBroadcastReceiver : BroadcastReceiver() {
     override fun onReceive(context: Context, intent: Intent) {
-        if (intent.action == Intent.ACTION_BOOT_COMPLETED) {
+        if (intent.action == Intent.ACTION_PACKAGE_REPLACED && Build.VERSION.SDK_INT < Build.VERSION_CODES.S && intent.dataString?.contains("com.greenart7c3.citrine") == true) {
+            val serviceIntent = Intent(context, WebSocketServerService::class.java)
+            context.startService(serviceIntent)
+        } else if (intent.action == Intent.ACTION_MY_PACKAGE_REPLACED) {
+            val serviceIntent = Intent(context, WebSocketServerService::class.java)
+            context.startService(serviceIntent)
+        } else if (intent.action == Intent.ACTION_BOOT_COMPLETED) {
             val serviceIntent = Intent(context, WebSocketServerService::class.java)
             context.startService(serviceIntent)
         }
