dadd1e1042b5c7644a92d4a03ee9de6dfcd66699
greenart7c3 <greenart7c3@proton.me>
2025-03-21 11:25:15 -0300

    Only allow reqs for private events when authenticated

--- app/src/main/java/com/greenart7c3/citrine/server/CommandResult.kt
+++ app/src/main/java/com/greenart7c3/citrine/server/CommandResult.kt
@@ -16,3 +16,16 @@ data class CommandResult(val eventId: String, val result: Boolean, val descripti
         fun invalid(event: Event, message: String) = CommandResult(event.id, false, "invalid: $message")
     }
 }
+
+data class ClosedResult(val subId: String, val message: String) {
+    fun toJson(): String {
+        return jacksonObjectMapper().writeValueAsString(
+            listOf("CLOSED", subId, message),
+        )
+    }
+
+    companion object {
+        fun required(subId: String) = ClosedResult(subId, "auth-required: we only accept events from registered users")
+        fun restricted(subId: String) = ClosedResult(subId, "restricted: we only accept events from registered users")
+    }
+}
--- app/src/main/java/com/greenart7c3/citrine/utils/Extensions.kt
+++ app/src/main/java/com/greenart7c3/citrine/utils/Extensions.kt
@@ -1,6 +1,9 @@
 package com.greenart7c3.citrine.utils
 
 import com.vitorpamplona.quartz.nip01Core.core.Event
+import com.vitorpamplona.quartz.nip04Dm.messages.PrivateDmEvent
+import com.vitorpamplona.quartz.nip59Giftwrap.wraps.GiftWrapEvent
+import com.vitorpamplona.quartz.nip78AppData.AppSpecificDataEvent
 
 private const val KIND_EVENT_DELETION = 5
 private val KINDS_EVENT_EPHEMERAL = 20000 until 30000
@@ -10,6 +13,7 @@ private val KINDS_EVENT_REPLACEABLE = setOf(
     10000 until 20000,
 )
 private val KINDS_PARAMETERIZED_REPLACEABLE = 30000 until 40000
+val KINDS_PRIVATE_EVENTS = setOf(PrivateDmEvent.KIND, GiftWrapEvent.KIND, AppSpecificDataEvent.KIND, 1060, 13004)
 
 fun Event.shouldDelete(): Boolean = kind == KIND_EVENT_DELETION
 fun Event.isEphemeral(): Boolean = KINDS_EVENT_EPHEMERAL.contains(kind)
