f5c0f21f66f25278a173f3fdd7ff99bd2d49dca9
greenart7c3 <greenart7c3@proton.me>
2025-03-21 07:04:21 -0300

    Fix fromjson

--- app/src/main/java/com/greenart7c3/citrine/server/CustomWebSocketServer.kt
+++ app/src/main/java/com/greenart7c3/citrine/server/CustomWebSocketServer.kt
@@ -120,7 +120,7 @@ class CustomWebSocketServer(
         val msgArray = EventMapper.mapper.readTree(newMessage)
         when (val type = msgArray.get(0).asText()) {
             "AUTH" -> {
-                val event = Event.fromJson(msgArray.get(1))
+                val event = Event.fromJson(msgArray.get(1).toString())
                 connection?.user = event.pubKey
                 connection?.session?.send(CommandResult.ok(event).toJson())
             }
