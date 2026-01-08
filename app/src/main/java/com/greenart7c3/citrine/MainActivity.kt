584366b19d444db35c44ce93125728f193cebf7f
greenart7c3 <greenart7c3@proton.me>
2026-01-08 07:00:02 -0300

    remove unused imports

--- app/src/main/java/com/greenart7c3/citrine/MainActivity.kt
+++ app/src/main/java/com/greenart7c3/citrine/MainActivity.kt
@@ -3,8 +3,6 @@ package com.greenart7c3.citrine
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.setValue
 import com.anggrayudi.storage.SimpleStorageHelper
 import com.greenart7c3.citrine.ui.CitrineScaffold
 import com.greenart7c3.citrine.ui.theme.CitrineTheme
