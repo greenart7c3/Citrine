439d5956dd027f3529b505626245d753278bc2f9
greenart7c3 <greenart7c3@proton.me>
2026-03-02 10:42:21 -0300

    Remove `busy_timeout` PRAGMA from database configuration

--- app/src/main/java/com/greenart7c3/citrine/database/AppDatabase.kt
+++ app/src/main/java/com/greenart7c3/citrine/database/AppDatabase.kt
@@ -50,7 +50,6 @@ abstract class AppDatabase : RoomDatabase() {
                     override fun onOpen(db: SupportSQLiteDatabase) {
                         super.onOpen(db)
                         db.execSQL("PRAGMA cache_size=-32000;")
-                        db.execSQL("PRAGMA busy_timeout=5000;")
                     }
                 })
                 .build()
