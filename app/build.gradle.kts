64f6005756488eee8ce53ab69ad623d7b6d6f5fd
greenart7c3 <greenart7c3@proton.me>
2026-09-18 06:30:41 -0300

    Bump version to 3.2.0 and update changelog

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "com.greenart7c3.citrine"
         minSdk = 26
         targetSdk = 36
-        versionCode = 63
-        versionName = "3.1.1"
+        versionCode = 64
+        versionName = "3.2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
